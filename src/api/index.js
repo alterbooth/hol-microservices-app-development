adfdaf7fca1a1429a9af573f004f946ff2323d06
tsubakimoto <tkmbmbr@gmail.com>
2019-06-13 06:47:17 +0900

    change api response

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
 app.get('/api/message', (req, res) => res.json({
-    'message': 'Hello world'
+    'message': 'Hello from Original API'
 }));
 
 app.listen(3000, () => console.log('Listening on port 3000'));
