fbe16ff8b95d773143ca8f8f6842b5bd5af053e3
tsubakimoto <tkmbmbr@gmail.com>
2019-06-15 11:23:05 +0900

    update web page

--- src/web/routes/index.js
+++ src/web/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  request({
+    url: process.env.API_ENDPOINT,
+    method: 'GET',
+    json: true
+  }, function (error, response, body) {
+    res.render('index', {
+      title: 'Microservices App Development Hands-on Lab',
+      message: body.message
+    });
+  });
 });
 
 module.exports = router;
