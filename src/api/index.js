b6ead1a456d84f2baa7d72f627e5e04644cb3be5
tsubakimoto <tkmbmbr@gmail.com>
2019-06-16 17:10:15 +0900

    refactoring

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
-
 const app = express();
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Origin', '*');
     next();
 });
 
-app.get('/api/v1/message', (req, res) => res.json({
-    message: 'Hello from API v1'
+app.get('/api/message', (req, res) => res.json({
+    message: 'Hello World v1'
 }));
 
 app.listen(3001, () => console.log('Listening on port 3001'));
