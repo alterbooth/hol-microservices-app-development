e0ba83a3bde3b1f1ed874af7111ca6c84e037968
tsubakimoto <tkmbmbr@gmail.com>
2019-06-16 17:10:24 +0900

    refactoring

--- src/web/routes/index.js
+++ src/web/routes/index.js
@@ -1,19 +1,18 @@
-var express = require('express');
-var router = express.Router();
-var request = require('request');
+const express = require('express');
+const router = express.Router();
+const request = require('request');
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
-  request({
-    url: process.env.API_ENDPOINT,
-    method: 'GET',
-    json: true
-  }, function (error, response, body) {
-    res.render('index', {
-      title: 'Microservices App Development Hands-on Lab',
-      message: body.message
+    request({
+        url: process.env.API_ENDPOINT,
+        method: 'GET',
+        json: true
+    }, function (error, response, body) {
+        res.render('index', {
+            title: 'Microservices App Development Hands-on Lab',
+            message: body.message
+        });
     });
-  });
 });
 
 module.exports = router;
